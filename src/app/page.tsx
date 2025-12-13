9532788527fba5e41ff28457cb349b4dd8693a41
codingyaari <codingyaari@gmail.com>
2025-12-13 15:06:14 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import { Footer } from "@/components/footer";
 export default function Home() {
   return (
     <main className="min-h-screen pb-24 relative">
-      <h1>Hello World</h1>
+      {/* <h1>Hello World</h1> */}
       <Navbar />
       <Profile />
       <About />
